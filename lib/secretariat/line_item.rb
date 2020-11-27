a63295089179eaac360237a4edc7ee296f2dadd0
Leonid Sobolievskyi <leonid@sobolievskyi.name>
2020-11-27 19:26:44 +0200

    validate rounded values

--- lib/secretariat/line_item.rb
+++ lib/secretariat/line_item.rb
@@ -61,7 +61,7 @@ module Secretariat
       end
 
       calculated_tax = (charge_price * BigDecimal(tax_percent) / BigDecimal(100)).round(2)
-      if calculated_tax != tax
+      if calculated_tax != tax.round(2)
         @errors << "Tax and calculated tax deviate: #{tax} / #{calculated_tax}"
         return false
       end
